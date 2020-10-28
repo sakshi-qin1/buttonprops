80b1bea86f2f0d8d9c0b3867685fa3020bc486f2
Sakshi Grover <51733354+sakshigrover-1998@users.noreply.github.com>
2020-10-28 14:24:46 +0530

    animation code

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export default [
+
+  {
+    css:
+      'url(https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',
+    height: 300
+  },
+  {
+    css:
+      'url(https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',
+    height: 300
+  },
+  {
+    css:
+      'url(https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',
+    height: 300
+  },
+  {
+    css:
+      'url(https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',
+    height: 300
+  },
+  
+]
